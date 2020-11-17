645229113398aa59fccce82540a3569463f39eff
cep104 <candicelizabethpeters@gmail.com>
2020-11-17 09:20:45 -0600

    notes added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const companyName = 'Scuber'; 
+// set to const so you can't change
+let mostProfitableNeighborhood = 'Chelsea';
+// set to let so you can change
+let companyCeo = 'Susan Smith'
+// set to let so you can change
